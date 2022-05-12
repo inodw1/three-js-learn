665bdb642da89a66c9cab9cc8a2423a91e16c656
inodw1 <inodwagachchi@gmail.com>
2022-05-12 19:55:50 +0530

    feat: test three js animations :construction:

---
+++ src/utils/main.js
@@ -0,0 +1,38 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+    // canvas: document.querySelector("#bg"),
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({
+    color: 0xff6347,
+    wireframe: true,
+});
+
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
+export function animate() {
+    requestAnimationFrame(animate);
+
+    torus.rotation.x += 0.01;
+    torus.rotation.Y += 0.005;
+    torus.rotation.Z += 0.01;
+
+    renderer.render(scene, camera);
+}
